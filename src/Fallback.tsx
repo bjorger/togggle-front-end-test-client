e88edef80228b69007a5c6a985e367a20ef5e86f
Robin Braumann <robin.braumann@gmail.com>
2022-04-27 12:33:38 +0200

    make token visible outside of console

--- src/Fallback.tsx
+++ src/Fallback.tsx
@@ -1,20 +1,36 @@
 import React from "react";
-import { useSearchParams } from "react-router-dom";
 import { oidcConfig } from "./App";
 import { OidcClient } from "oidc-client";
 
 const Fallback: React.FC = () => {
-    let [searchParams] = useSearchParams();
-    const oidcClient = new OidcClient(oidcConfig);
+    const [token, setToken] = React.useState<string>("");
+    const [identity, setIdentity] = React.useState<string>("");
 
-    (async () => {
-        const response = await oidcClient.processSigninResponse(window.location.href);
-        const idToken = response.profile;
-        console.log(idToken.sub);
-        console.log(response.access_token);
-    })();
+    React.useEffect(() => {
+        (async () => {
+            const oidcClient = new OidcClient(oidcConfig);
 
-    return <div></div>;
+            const response = await oidcClient.processSigninResponse(window.location.href);
+            const idToken = response.profile;
+
+            console.log(idToken.sub);
+            console.log(response.access_token);
+
+            setIdentity(idToken.sub);
+            setToken(response.access_token);
+        })();
+    }, [token, identity]);
+
+    return (
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+            <p style={{ textAlign: "center", margin: "50px 0", width: "500px", wordBreak: "break-all" }}>
+                Identity: <br /> {identity} <br /> <br />
+                token: <br />
+                <br />
+                {token}
+            </p>
+        </div>
+    );
 };
 
 export default Fallback;
